156f6af0d45ca1e797978007f993f89325a912c5
giacomoverdura <github.com/giacomoverdura>
2026-04-06 00:03:12 +0800

    create: tech stack ui pages

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { About, Contact, Footer, Header, Projects, Stack } from '../routes/Navig
 
 export default function Index() {
   return (
-    <div className="max-w-[1200px] w-full mx-auto px-6 md:px-20 lg:px-40 pt-10 md:pt-20">
+    <div className="max-w-[1200px] w-full mx-auto px-6 md:px-20 lg:px-40 pt-10 md:pt-10 animate-fadeIn">
 
       <Header />
       <About />
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <div className="not-found">
-      <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <h1 className="text-8xl font-bold text-green-700">404 Not Found</h1>
+        <p className="mt-4 text-lg">The page you are looking for does not exist.</p>
+      </div>
     </div>
   );
 }
